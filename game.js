72930871bf7dd270fc7c09ecaabbe826467d5722
pratyaksh03 <veerbhuria05@gmail.com>
2023-07-06 17:30:16 +0530

    Complete Assignment

--- game.js
+++ game.js
@@ -1,15 +1,15 @@
 let i = 0;
-var playerchoice;
-var computerchoice;
+let playerchoice;
+let computerchoice;
 
 function getComputerChoice() {
-    if (i == 0) {
+    if (i === 0) {
         computerchoice = "rock";
         i += 1;
-    } else if (i == 1) {
+    } else if (i === 1) {
         computerchoice = "paper";
         i += 1;
-    } else if (i == 2) {
+    } else if (i === 2) {
         computerchoice = "scissors";
         i += 1;
     } else {
@@ -17,28 +17,70 @@ function getComputerChoice() {
     }
 }
 
-function round() {
-    if (playerchoice !== "rock" || "scissors" || "paper") {
-        alert("Invalid Play");
-    }
-    else if (playerchoice == "rock" && computerchoice == "scissors") {
-        alert("You Won!");
-    } else if (playerchoice == "paper" && computerchoice == "rock") {
-        alert("You Won!");
-    } else if (playerchoice == "scissors" && computerchoice == "paper") {
-        alert("You won!");
-    } else {
-        alert("You lose!");
-    }
-}
+var rock = document.querySelector('#rock');
+rock.addEventListener('click', function() {
+  playerchoice = 'rock';
+  if (j < 5 && k < 5) {
+    game();
+  } else if (j == 5) {
+    alert("Player Won! Score " + j + "-" + k);
+  } else {
+    alert("Computer Won! Score " + j + "-" + k);
+  }
+});
+
+var paper = document.querySelector('#paper');
+paper.addEventListener('click', function() {
+  playerchoice = 'paper';
+  if (j < 5 && k < 5) {
+    game();
+  } else if (j == 5) {
+    alert("Player Won! Score " + j + "-" + k);
+  } else {
+    alert("Computer Won! Score " + j + "-" + k);
+  }
+});
+
+var scissor = document.querySelector('#scissor');
+scissor.addEventListener('click', function() {
+  playerchoice = 'scissor';
+  if (j < 5 && k < 5) {
+    game();
+  } else if (j == 5) {
+    alert("Player Won! Score " + j + "-" + k);
+  } else {
+    alert("Computer Won! Score " + j + "-" + k);
+  }
+});
+
+
+let j = 1;
+let k = 1;
 
 function game() {
-    for (let j = 0; j < 5; j++) {
-        playerchoice = String(prompt("Your play: "));
-        playerchoice = playerchoice.toLowerCase();
-        getComputerChoice();
-        round();
+    getComputerChoice();
+
+    if (playerchoice === "rock" && computerchoice === "scissors") {
+        alert("Player +1");
+        j++;
+    } else if (playerchoice === "paper" && computerchoice === "rock") {
+        alert("Player +1");
+        j++;
+    } else if (playerchoice === "scissors" && computerchoice === "paper") {
+        alert("Player +1");
+        j++;
+    } else {
+        alert("Computer +1");
+        k++;
     }
 }
 
-game();
+var resetButton = document.querySelector('#reset');
+resetButton.addEventListener('click', function() {
+  // Reset scores and game state
+  j = 1;
+  k = 1;
+  i = 0;
+  playerchoice = null;
+  computerchoice = null;
+});
